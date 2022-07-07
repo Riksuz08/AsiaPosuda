b95cdcb53d7d327f9db7852c759eb16950b845d3
Sunnatillo Shavkatov <sunnatillo.shavkatov@udevs.io>
2022-07-07 14:34:19 +0500

    few change

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -31,46 +31,48 @@ class MainApp extends StatelessWidget {
         isTestMode: true,
       ),
       child: KeyboardDismisser(
-        child: Builder(builder: (ctx) {
-          final options = GalleryOptions.of(ctx);
-          return MultiBlocProvider(
-            providers: [
-              BlocProvider<MainBloc>(create: (_) => MainBloc()),
-            ],
-            child: MaterialApp(
-              title: "Sample App",
-              scrollBehavior:
-                  const MaterialScrollBehavior().copyWith(scrollbars: false),
-              debugShowCheckedModeBanner: false,
-              navigatorKey: AppConstants.navigatorKey,
-              scaffoldMessengerKey: AppConstants.scaffoldMessengerKey,
-
-              /// theme style
-              theme: AppThemes.light,
-              darkTheme: AppThemes.dark,
-              themeMode: options.themeMode,
-
-              /// lang
-              locale: options.locale,
-              localizationsDelegates: const [
-                AppLocalizations.delegate,
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-                DefaultCupertinoLocalizations.delegate,
+        child: Builder(
+          builder: (ctx) {
+            final options = GalleryOptions.of(ctx);
+            return MultiBlocProvider(
+              providers: [
+                BlocProvider<MainBloc>(create: (_) => MainBloc()),
               ],
-              supportedLocales: const [
-                Locale('ru', 'RU'),
-                Locale('uz', 'UZ'),
-                Locale('en', 'US'),
-              ],
+              child: MaterialApp(
+                title: "Sample App",
+                scrollBehavior:
+                    const MaterialScrollBehavior().copyWith(scrollbars: false),
+                debugShowCheckedModeBanner: false,
+                navigatorKey: AppConstants.navigatorKey,
+                scaffoldMessengerKey: AppConstants.scaffoldMessengerKey,
 
-              /// pages
-              routes: AppPages.routes,
-              initialRoute: AppRoutes.initial,
-            ),
-          );
-        }),
+                /// theme style
+                theme: AppThemes.light,
+                darkTheme: AppThemes.dark,
+                themeMode: options.themeMode,
+
+                /// lang
+                locale: options.locale,
+                localizationsDelegates: const [
+                  AppLocalizations.delegate,
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate,
+                  DefaultCupertinoLocalizations.delegate,
+                ],
+                supportedLocales: const [
+                  Locale('ru', 'RU'),
+                  Locale('uz', 'UZ'),
+                  Locale('en', 'US'),
+                ],
+
+                /// pages
+                routes: AppPages.routes,
+                initialRoute: AppRoutes.initial,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
